a57eb75782a5e09a5318b4d515582e2ea0a4938b
jinwoo park <swat018@gmail.com>
2019-01-24 10:29:24 +0900

    자바의신(god of java) 예제파일

---
+++ godofjava/src/com/godofjava/first/calculator.java
@@ -0,0 +1,10 @@
+package com.godofjava.first;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Calculator class started");
+	}
+
+}
