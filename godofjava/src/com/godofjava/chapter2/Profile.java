aa66d7fbac2b3e2d05a23c71cede3eb2c5f79000
jinwoo park <swat018@gmail.com>
2019-02-02 10:28:46 +0900

    godofjava chapter1~3

---
+++ godofjava/src/com/godofjava/chapter2/Profile.java
@@ -0,0 +1,28 @@
+package com.godofjava.chapter2;
+
+public class Profile {
+	String name;
+	int age;
+	
+	public void setName(String str) {
+		name = str;
+	}
+	public void setAge(int val) {
+		age = val;
+	}
+	public void printName() {
+		System.out.println("Mt name is "+name);
+	}
+	public void printAge() {
+		System.out.println("My age is "+age);
+	}
+	
+	public static void main(String[] args) {
+		Profile profile = new Profile();
+		profile.setName("Min");
+		profile.setAge(20);
+		profile.printName();
+		profile.printAge();
+	}
+
+}
---
+++ godofjava/src/com/godofjava/chapter3/Calculator.java
@@ -0,0 +1,29 @@
+package com.godofjava.chapter3;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Calculator class started");
+		Calculator calc = new Calculator();
+		int a = 10;
+		int b = 5;
+		System.out.println("add="+calc.add(a, b));
+		System.out.println("subtract="+calc.subtract(a, b));
+		System.out.println("multiply="+calc.multiply(a, b));
+		System.out.println("divide="+calc.divide(a, b));
+	}
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
