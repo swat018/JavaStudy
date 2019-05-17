0c0251f9f7d10227af721826f91b582736007c3d
jinwoo park <swat018@gmail.com>
2019-05-17 20:18:13 +0900

    godofjava chapter 22 List

---
+++ godofjava/src/com/godofjava/chapter22/StackSample.java
@@ -0,0 +1,30 @@
+package com.godofjava.chapter22;
+
+import java.util.Stack;
+
+public class StackSample {
+	public static void main(String[] args) {
+		StackSample sample = new StackSample();
+		sample.checkPeek();
+		sample.checkPop();
+	}
+	
+	public void checkPeek() {
+		Stack<Integer> intStack = new Stack<Integer>();
+		for(int loop=0; loop<5; loop++) {
+			intStack.push(loop);
+			System.out.println(intStack.peek());
+		}
+		
+		System.out.println("size = " + intStack.size());
+	}
+	public void checkPop() {
+		Stack<Integer> intStack = new Stack<Integer>();
+		for(int loop=0; loop<5; loop++) {
+			intStack.push(loop);
+			System.out.println(intStack.pop());
+		}
+		
+		System.out.println("size = " + intStack.size());
+	}
+}
